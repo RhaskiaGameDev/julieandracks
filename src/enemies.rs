89941e533e74630125877d83943bf18a27946046
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2023-06-19 19:33:57 -0700

    changes

--- src/enemies.rs
+++ src/enemies.rs
@@ -5,11 +5,7 @@ pub(crate) fn spawn_enemies(mut commands: Commands, asset_server: Res<AssetServe
     let mut rng = rand::thread_rng();
     commands.spawn(SpriteBundle {
         texture: asset_server.load("fly.png"),
-        transform: Transform::from_translation(Vec3::new(
-            rng.gen_range(-240.0..240.0),
-            rng.gen_range(-135.0..135.0),
-            0.,
-        )),
+        transform: Transform::from_translation(Vec3::new(rng.gen_range(-240.0..240.0), -130., 0.)),
         ..default()
     });
 }
